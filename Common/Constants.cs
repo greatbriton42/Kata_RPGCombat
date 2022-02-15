985fb0e7c45b208f569c97720a15dd87e4fb0e02
Briton Z Kelley <briton.kelley@cgius.com>
2022-02-14 21:57:00 -0600

    Iteration One

---
+++ Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Constants
+    {
+        public const int MAX_HEALTH = 1000;
+        public const int BASE_ATTACK_DAMAGE = 50;
+        public const int BASE_HEALING_VALUE = 30;
+    }
+}
---
+++ Domain/Character.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Character
+    {
+        public Guid ID { get; }
+        public int Health { get; set; }
+        public bool IsAlive { get; set; }
+        public int Level { get; set; }
+        public int AttackDamage { get; set; }
+        public int HealingValue { get; set; }
+
+        public Character(bool isAlive = true)
+        {
+            ID = Guid.NewGuid();
+            IsAlive = isAlive;
+            Health = IsAlive ? Constants.MAX_HEALTH: 0;
+            Level = 1;
+            AttackDamage = Constants.BASE_ATTACK_DAMAGE;
+            HealingValue = Constants.BASE_HEALING_VALUE;
+
+        }
+        public int Damage(Character character)
+        {
+            character.Health -= AttackDamage;
+            if(character.Health <= 0)
+            {
+                character.Health = 0;
+                character.IsAlive = false;
+            }
+            return AttackDamage;
+        }
+        public int Heal(Character character)
+        {
+            if (character.IsAlive)
+            {
+                character.Health = character.Health + HealingValue > Constants.MAX_HEALTH ? Constants.MAX_HEALTH : character.Health + HealingValue;
+            }
+            return HealingValue;
+        }
+    }
+}
